7bea4ba7ea498aca0ab88f45653d56d39de51078
Chamon562 <Chamon562@gmail.com>
2021-06-30 16:46:28 -0700

    Add styled components for Nav, NavLink, and Logo

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,15 +1,33 @@
 import React from 'react';
-import styled from 'styled-components';
-import {Link} from 'react-router-dom';
-import {menuData} from '../data/MenuData';
+import styled, { css } from 'styled-components/macro';
+import { Link } from 'react-router-dom';
+import { menuData } from '../data/MenuData';
 
 const Nav = styled.nav`
     height: 60px;
-    background: red;
+    display: flex;
+    justify-content: space-between;
+    padding: 1rem 2rem;
+    z-index: 100;
+    position: fixed;
+    width: 100%;
+    background: #000;
 `
-const Logo = styled(Link)`
+const NavLink = css`
     color: #fff;
+    display: flex;
+    align-items: center;
+    padding: 0 1rem;
+    height: 100%;
+    cursor: pointer;
 `
+
+const Logo = styled(Link)`
+    ${NavLink};
+    color: #fff;
+    font-style: italic; 
+`
+
 
 const MenuBars = styled.i`
 
@@ -19,7 +37,7 @@ const NavMenu = styled.div`
 `
 
 const NavMenuLinks = styled(Link)`
-
+    color: #fff;
 `
 const Navbar = () => {
     return (
@@ -27,11 +45,12 @@ const Navbar = () => {
             <Logo to="/">ADM</Logo>
             <MenuBars />
             <NavMenu>
-                {menuData.map((item, index) =>{
-                    <NavMenuLinks to={item.link} key ={index}>
+                {menuData.map((item, index) => (
+                    <NavMenuLinks to={item.link} key={index}>
                         {item.title}
                     </NavMenuLinks>
-                })}
+                )
+                )}
             </NavMenu>
             <h1>Navbar</h1>
         </Nav>
