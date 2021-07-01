a0af2237cc62bf5ff037fff0e84054159538391b
Chamon562 <Chamon562@gmail.com>
2021-07-01 14:16:17 -0700

    Add Monsterrat from google fonts link to index.html in public folder.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Navbar from './components/Navbar';
+import GlobalStyle from './globalStyles';
 
 
 function App() {
   return (
     <>
-     <Navbar/>
+      <GlobalStyle />
+      <Navbar />
     </>
   );
 }
