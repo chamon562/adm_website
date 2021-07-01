472f7424f1ec9e4a784a76dc26d442fc65e672e3
Chamon562 <Chamon562@gmail.com>
2021-07-01 14:55:56 -0700

    [SliderData.js] Add array of objects of some data to be used later in the hero section.

---
+++ src/data/SliderData.js
@@ -0,0 +1,12 @@
+// this is where will have all the data for slider to be exported for us
+export const SliderData = [
+    {
+
+        title: 'This Person Name, Or This event here',
+        price: '$500,000',
+        location: '5555 Bloom field',
+        path: '/our-team',
+        image: ImageOne,
+        alt: 'Event or ...'
+    }
+]
