85676b7c9a5a1b6707bdca1775074e4542118413
Chamon562 <Chamon562@gmail.com>
2021-07-01 14:47:07 -0700

    [Hero.js] Add wrapper css to see what the hero section would look like.

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,12 +1,30 @@
 import React from 'react'
 import styled, { css } from 'styled-components/macro';
 
+const HeroSection = styled.section`
+    height: 100vh;
+    max-height: 1100px;
+    position: relative; 
+    overflow: hidden;
+`
+// wrapper next
+const HeroWrapper = styled.div`
+    width: 100%;
+    height: 100%;
+    display: flex; 
+    justify-content: center;
+    align-items: center;
+    overflow: hidden;
+    position: relative; 
+`
 
 const Hero = () => {
     return (
-        <div>
+        <HeroSection>
+            <HeroWrapper>
             <h1>Hero Section</h1>
-        </div>
+            </HeroWrapper>
+        </HeroSection>
     )
 }
 
